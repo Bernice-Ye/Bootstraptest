8e50c84487ff5d5685e41419866954074131f167
BerniceYeh <bernice.yeh@jetfusion.com.tw>
2017-03-15 18:40:39 +0800

    加入已練習檔案

---
+++ test/Controllers/BootstrapBerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class BootstrapBerController : Controller
+    {
+        // GET: BootstrapBer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return PartialView("_Home");
+        }
+        public ActionResult Messages()
+        {
+            return PartialView("_Messages");
+        }
+        public ActionResult Introduce()
+        {
+            return PartialView("_Introduce");
+        }
+    }
+}
